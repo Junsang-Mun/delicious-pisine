f9d937da3ebd1f655c362fbe56535ee1efe5badc
Junsang Mun <mun.js@aol.com>
2022-08-12 14:54:21 +0900

    repo init

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<div class="navbar bg-base-100 shadow-lg m-5 rounded-lg">
+    <a class="btn btn-ghost normal-case text-xl" href="http://localhost:5173">daisyUI</a>
+</div>
+
+<div class="m-10">
+    <button class="btn btn-error">button</button>
+</div>
