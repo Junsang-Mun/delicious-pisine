74c055d4ac63dc03bafad0eedefde64ef436cf94
Junsang Mun <mun.js@aol.com>
2022-08-12 20:21:58 +0900

    뭐가 문제지

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,40 @@
 <script>
-import { dotenv } from "dotenv";
+import dotenv from "dotenv";
 import { onMount } from "svelte";
-import { apiData, drinkNames } from './store.js';
+import { writable } from "svelte/store";
+//import { apiData, drinkNames } from "./store.js";
 dotenv.config();
 
 const databaseId = process.env.DB_ID;
 const apiKey = process.env.API_KEY;
+const url = `https://api.notion.com/v1/databases/${databaseId}/query`;
+export const apiData = writable([]);
 
 onMount(async () => {
-  fetch("https://api.notion.com/v1/databases/" + apiKey)
+  const data = await(
+    await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+      },
+      method: "POST",
+    }).catch(error => {
+      console.log(error);
+    })
+  ).json();
+  apiData.set(data);
+});
+
+  /*
+  fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+      "Notion-Version": "2022-06-28"
+    },
+    method: "POST",
+  })
   .then(response => response.json())
   .then(data => {
 		console.log(data);
@@ -17,15 +43,15 @@ onMount(async () => {
     console.log(error);
     return [];
   });
-});
+}); */
 </script>
 
 
 
 <div class="navbar bg-base-100 shadow-lg bg-base-200">
-  <a class="btn btn-ghost normal-case text-xl" href="http://localhost:5173">daisyUI</a>
+  <a class="btn btn-ghost normal-case text-xl" href="http://localhost:5173">{apiData}</a>
 </div>
-
+<!--
 <div class="m-10">
     <h1>Whiskey Drinks Menu</h1>
     {#each $drinkNames as drinkName}
@@ -41,3 +67,4 @@ onMount(async () => {
     </div>
     {/each}
 </div>
+-->
