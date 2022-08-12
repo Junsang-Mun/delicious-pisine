3ca10bc92c9248ffbb3229998c8a988cfa3ad642
Junsang Mun <mun.js@aol.com>
2022-08-12 15:46:46 +0900

    Add deafult card frame
    - Added card frame w/ image & button
    - TODO; API Integration

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,16 @@
-<div class="navbar bg-base-100 shadow-lg m-5 rounded-lg">
+<div class="navbar bg-base-100 shadow-lg bg-base-200">
     <a class="btn btn-ghost normal-case text-xl" href="http://localhost:5173">daisyUI</a>
 </div>
 
 <div class="m-10">
-    <button class="btn btn-error">button</button>
+  <div class="card lg:card-side bg-base-100 shadow-xl m-10">
+    <figure><img src="https://placeimg.com/400/400/arch" alt="Album"></figure>
+    <div class="card-body">
+      <h2 class="card-title">New album is released!</h2>
+      <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Listen</button>
+      </div>
+    </div>
+  </div>
 </div>
