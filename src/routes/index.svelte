0ba7409927dc16f0cd486673e6de9d2c93950fb3
Junsang Mun <mun.js@aol.com>
2022-08-13 17:00:17 +0900

    뭔가 작업을 한 것 같긴 한데
    - auth가 불가능함

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { getDB } from './variable';
+    //import 'dotenv/config'
+
+    onMount(async () => {
+        console.log(import.meta.env.VITE_DB_ID);
+        console.log(import.meta.env.VITE_NOTION_API_KEY);
+
+        fetch(`https://api.notion.com/v1/databases/${import.meta.env.VITE_DB_ID}/query`, {
+            headers: {
+                Authorization: `Bearer ${import.meta.env.VITE_NOTION_API_KEY}`,
+                'Content-Type': 'application/json',
+                'Notion-Version': '2022-06-08'
+            },
+            method: 'POST',
+            mode: 'no-cors'
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        });
+    })
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/variable.ts
@@ -0,0 +1,5 @@
+//import { DB_ID, VITE_API_KEY } from '$env/static/public';
+
+export function getDB() {
+  console.log(import.meta.env.DB_ID);
+}
