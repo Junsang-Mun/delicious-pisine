140def7f7212d1cafbc42eb3f96d3f3272938c53
Junsang Mun <mun.js@aol.com>
2022-08-19 23:55:58 +0900

    Cleanup unnessery files and updates on main.svelte
    - 가격 인디케이터를 설정했습니다.

--- src/routes/components/Main.svelte
+++ src/routes/components/Main.svelte
@@ -21,43 +21,27 @@
 </script>
 
 <main>
-    <p></p>
 <div>
-    {#each restaurantDatas as rData}
+    {#each restaurantDatas as rd}
     <div class="card w-96 bg-base-100 shadow-xl m-10">
         <div class="card-body">
-          <h2 class="card-title">{JSON.stringify(rData.attributes.Name).replace(/\"/g, '')}</h2>
-          <p>{@html JSON.stringify(rData.attributes.Menu).replace(/\\n/g, "<br />").replace(/\"/g, '')}</p>
+          <h2 class="card-title">{JSON.stringify(rd.attributes.Name).replace(/\"/g, '')}</h2>
+          <p>{@html JSON.stringify(rd.attributes.Menu).replace(/\\n/g, "<br />").replace(/\"/g, '')}</p>
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <p class="btn btn-outline">{JSON.stringify(rd.attributes.Kind).replace(/\"/g, '')}</p>
+            {#if JSON.stringify(rd.attributes.Price).replace(/\"/g, '') == 'High'}
+                <button class="btn btn-outline btn-error no-animation">가격 높음</button>
+            {:else if JSON.stringify(rd.attributes.Price).replace(/\"/g, '') == 'Mid'}
+                <button class="btn btn-outline btn-warning no-animation">가격 보통</button>
+            {:else if JSON.stringify(rd.attributes.Price).replace(/\"/g, '') == 'Low'}
+                <button class="btn btn-outline btn-success no-animation">가격 낮음</button>
+            {:else}
+                <button class="btn btn-outline no-animmation">가격 정보 없음</button>
+            {/if}
+            <button class="btn btn-primary">자세히 보기</button>
           </div>
         </div>
       </div>
     {/each}
 </div>
 </main>
-
-
-
-
-
-
-
-<!-- <script>
-    import { onMount } from 'svelte';
-    import { apiCall } from '../functions/apicall'
-    let restaurants = [];
-
-    onMount(async () => {
-        apiCall();
-        console.log('asdfasdf');
-        console.log('dd');
-    });
-</script>
-
-<div>
-    {#each restaurants as data}
-	<pre>{data.data.id}</pre>
-    {/each}
-</div>
- -->
