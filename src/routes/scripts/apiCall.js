57ae03e757d02d8db74e309785848f86588b7984
Junsang Mun <mun.js@aol.com>
2022-08-21 00:39:38 +0900

    randomSort 대응

--- src/routes/scripts/apiCall.js
+++ src/routes/scripts/apiCall.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { restaurantDatas } from './store';
 
-const url = 'https://delicious-pisine-backend.herokuapp.com/api/restaurants';
+const url = 'https://delicious-pisine-backend.herokuapp.com/api/restaurants?randomSort=true';
 
 export function apiCall() {
     axios.get(url)
@@ -14,7 +14,7 @@ export function apiCall() {
     })
 }
 export function apiCallFilter(k, v){
-    axios.get(`${url}?filters[${k}][$eq]=${v}`)
+    axios.get(`${url}&filters[${k}][$eq]=${v}`)
     .then(response => {
         //console.log(response.data.data);
         restaurantDatas.set(response.data.data);
