b9b9bb4c89b204653d184ebcf777dc0ebf891b7c
Junsang Mun <mun.js@aol.com>
2022-08-20 15:31:16 +0900

    fix lat/lng to give directions for map

--- src/routes/scripts/giveMeWay.js
+++ src/routes/scripts/giveMeWay.js
@@ -1,10 +1,14 @@
+// lat/lng 관련 Note:
+// 신경쓸 필요 없음
+// DB상 저장이 잘못되어서 lat/lng 이슈는 Main.svelte에서만 확인하면 괜찮음
+
 export function giveMeWay(lat, lng, provider, name) {
     const s_lng = '127.029776';
     const s_lat = '37.4920509';
 
     if (provider === 'N') {
         //naver map
-        const url = `nmap://route/walk?slat=${s_lat}&slng=${s_lng}&sname=피씨너들의_무덤&dlat=37.4764356&dlng=126.9618302&dname=${name}`
+        const url = `nmap://route/walk?slat=${s_lat}&slng=${s_lng}&sname=피씨너들의_무덤&dlat=${lat}&dlng=${lng}&dname=${name}`
         window.open(url);
     } else if (provider === 'K') {
         //kakao map
