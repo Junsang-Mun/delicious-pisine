b98aa6476655d5ca7bf3953c131949f9dcabbd43
Junsang Mun <mun.js@aol.com>
2022-11-22 18:27:59 +0900

    [feature] header에 위치(저장), 정렬기능 추가
    - 위치 선택 -> cookieAgree라면 localStorage에 저장
    - 정렬 기능 추가

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,21 +1,81 @@
 <script>
-	import { listData } from '../scripts/api';
+	import { onMount } from 'svelte';
+	import { listData, sortData } from '../scripts/api';
+	import { getLocation, setLocation, cookieAgreement } from '../scripts/cookie';
 	import img1 from '../images/1.png';
 	import img2 from '../images/2.png';
+	let locatoinFlag = '';
+	let currentLocation = ''
+	let userAgreedForCookie = false;
+	function sort(opt) {
+		sortData(opt);
+	}
+	function location(opt) {
+		userAgreedForCookie = cookieAgreement();
+		if (opt === 'gaepo') {
+			if (userAgreedForCookie) {
+				setLocation('gaepo');
+			}
+			locatoinFlag = '개포';
+			currentLocation = 'gaepo';
+		} else if (opt === 'seocho') {
+			if (userAgreedForCookie) {
+				setLocation('seocho');
+			}
+			locatoinFlag = '서초';
+			currentLocation = 'seocho';
+		}
+	}
+	onMount(() => {
+		const loc = getLocation();
+		if (loc == 'gaepo') {
+			locatoinFlag = '개포';
+			location('gaepo');
+		} else if (loc == 'seocho') {
+			locatoinFlag = '서초'
+			location('seocho');
+		} else {
+			locatoinFlag = '개포'
+			location('gaepo');
+		}
+		userAgreedForCookie = cookieAgreement();
+	})
 </script>
 
-<div class="navbar bg-base-100 shadow mb-4">
+<div class="navbar bg-base-100">
 	<div class="flex-1">
-		<button class="btn btn-ghost normal-case text-xl" on:click="{() => listData()}">존맛탱 피씬</button>
-		<input type="checkbox" class="toggle" checked />
+		<a class="btn btn-ghost normal-case text-xl" on:click={() => listData()}>daisyUI</a>
 	</div>
 	<div class="flex-none">
-		<label for="modal-help" class="btn btn-square btn-ghost modal-button">
-			<svg xmlns="http://www.w3.org/2000/svg" width="70%" height="70%" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></circle><circle cx="128" cy="180" r="12"></circle><path d="M128,144v-8a28,28,0,1,0-28-28" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
-		</label>
-		<input type="checkbox" id="modal-help" class="modal-toggle" />
-
-		<!-- 사용설명서 시작 -->
+		<ul class="menu menu-horizontal p-0">
+			<li tabindex="0">
+				<a>
+					{locatoinFlag}
+					<svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+				</a>
+				<ul class="p-2 bg-base-100 z-50 border-2 border-base-300">
+					<li><a on:click={() => location('gaepo')}>개포 클러스터</a></li>
+					<li><a on:click={() => location('seocho')}>서초 클러스터</a></li>
+				</ul>
+			</li>
+			<li tabindex="0">
+				<a>
+					정렬 옵션
+					<svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+				</a>
+				<ul class="p-2 bg-base-100 z-50 border-2 border-base-300">
+					<li><a on:click={() => sort('P_LTH')}>가격 낮은 순</a></li>
+					<li><a on:click={() => sort('P_HTL')}>가격 높은 순</a></li>
+					<li><a on:click={() => sort('A_ATZ')}>가게 이름 오름차순</a></li>
+					<li><a on:click={() => sort('A_ZTA')}>가게 이름 내림차순</a></li>
+					<li><a on:click={() => sort('C_ATZ')}>카테고리별로 묶기</a></li>
+				</ul>
+			</li>
+			<li><a>Item 3</a></li>
+		</ul>
+	</div>
+</div>
+		<!-- 사용설명서 시작 --
 		<label for="modal-help" class="modal cursor-pointer">
 			<label class="modal-box relative" for="">
 				<label for="modal-help" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
@@ -32,6 +92,4 @@
 				<p class="py-4">필터를 해제하고 싶으시면 상단의 <b>존맛탱 피씬</b>을 클릭해 주세요.</p>
 			</label>
 		</label>
-		<!-- 사용설명서 끝 -->
-	</div>
-</div>
+		사용설명서 끝 -->
