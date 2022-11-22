0e17b54f75776dfe71cfb3d793eb5521c6c1464b
Junsang Mun <mun.js@aol.com>
2022-11-22 17:15:17 +0900

    [feature] direct하게 cookieAgreement 부르지 않도록 로직 개선
    - userSaidYse/No 함수에서 displayCookieAlart 값 조작하고 cookieAgreement 부르도록 개선

--- src/routes/components/Main.svelte
+++ src/routes/components/Main.svelte
@@ -7,13 +7,20 @@
 	import { giveMeWay } from '../scripts/giveMeWay';
 	let userAgreedForCookie = false;
 	let displayCookieAlert = false;
+	function userSaidYes() {
+		cookieAgreement(true);
+		displayCookieAlert = false;
+	}
+	function userSaidNo() {
+		cookieAgreement(false);
+		displayCookieAlert = false;
+	}
 	onMount(() => {
 		listData();
 		userAgreedForCookie = cookieAgreement();
 		if (userAgreedForCookie !== true) {
 			displayCookieAlert = true;
 		}
-		console.log(userAgreedForCookie);
 	})
 </script>
 
@@ -38,14 +45,14 @@
 		</div>
 	</div>
 	{#if displayCookieAlert === true}
-		<div class="alert shadow-lg fixed z-50 max-w-4xl bottom-10 left-10">
+		<div class="alert shadow-lg fixed z-50 max-w-xl bottom-10 left-10">
 			<div>
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 				<span>we use cookies for no reason.</span>
 			</div>
 			<div class="flex-none">
-				<button class="btn btn-sm btn-ghost" on:click|preventDefault={() => cookieAgreement(false)}>Deny</button>
-				<button class="btn btn-sm btn-primary" on:click|preventDefault={() => cookieAgreement(true)}>Accept</button>
+				<button class="btn btn-sm btn-ghost" on:click|preventDefault={() => userSaidNo()}>Deny</button>
+				<button class="btn btn-sm btn-primary" on:click|preventDefault={() => userSaidYes(true)}>Accept</button>
 			</div>
 		</div>
 	{/if}
