fe88df70a6a5230ec4345c4d3426ce89149962a2
Junsang Mun <mun.js@aol.com>
2022-11-24 18:21:59 +0900

    [WIP]

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
-	import { listData, sortData } from '../scripts/api';
-	import { getLocation, setLocation } from '../scripts/cookie';
+	import { sortData } from '../scripts/api';
+	import { getLocation, setLocation } from '../scripts/localStorage';
+	import logo from '../images/42delicious.png';
 	let locatoinFlag = '';
 	function sort(opt) {
 		sortData(opt);
@@ -37,7 +38,7 @@
 
 <div class="navbar bg-base-100">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl" on:click={() => listData()}>daisyUI</a>
+		<a><img src={logo} class="h-12 pl-2"/></a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal p-0">
@@ -64,25 +65,7 @@
 					<li><a on:click={() => sort('C_ATZ')}>카테고리별로 묶기</a></li>
 				</ul>
 			</li>
-			<li><a>Item 3</a></li>
+			<li><a>추천받기</a></li>
 		</ul>
 	</div>
 </div>
-		<!-- 사용설명서 시작 --
-		<label for="modal-help" class="modal cursor-pointer">
-			<label class="modal-box relative" for="">
-				<label for="modal-help" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-				<h2 class="text-lg font-bold">존맛탱 피씬 사용설명서</h2>
-				<img src="{img1}" alt="맥도날드 서초뱅뱅점 | 피씨너의 마음의 고향 McDonald's | 패스트푸드 | 가격 낮음 | 자세히 보기"/>
-				<p class="py-4">상단의 제목은 음식점 이름을 나타냅니다.</p>
-				<p class="py-1">하단의 버튼 세 개는 각각 '<b>음식점 종류</b>', '<b>가격대</b>' 및 '<b>상세정보</b>'를 나타내며, 모든 버튼은 누를 수 있습니다.</p>
-				<p class="py-1"><b>음식점 종류</b>를 누르면 동일한 종류의 음식점을 모아서 보여줍니다.</p>
-				<p class="py-1"><b>가격대</b>를 누르면 동일한 종류의 음식점을 모아서 보여줍니다.</p>
-				<img src="{img2}" alt=" " class="py-4" />
-				<p class="py-4"><b>상세정보</b>를 누르면 간략한 메뉴와 음식점의 위치, 영업시간 및 기타 정보가 나타납니다.</p>
-				<p class="py-1">네이버 지도와 카카오맵을 통해 바로 길을 찾을 수 있습니다.</p>
-				<p class="py-1">모바일에서는 애플리케이션이, PC에서는 웹이 열립니다.</p>
-				<p class="py-4">필터를 해제하고 싶으시면 상단의 <b>존맛탱 피씬</b>을 클릭해 주세요.</p>
-			</label>
-		</label>
-		사용설명서 끝 -->
--- src/routes/components/Main.svelte
+++ src/routes/components/Main.svelte
@@ -4,6 +4,11 @@
 	import { listData } from '../scripts/api';
 	import { restaurantDatas, showThisLocation } from '../scripts/store';
 	let displayLocation;
+	let forceUpdate;
+	let displayFlag = 'LIST';
+	restaurantDatas.subscribe(() => {
+		forceUpdate = {};
+	})
 	showThisLocation.subscribe(loc => {
 		displayLocation = loc;
 	})
@@ -13,17 +18,19 @@
 </script>
 
 <main>
+	{#if displayFlag === 'LIST'}
 	<div>
 		<div class="container mx-auto">
 			<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-				{#key displayLocation}
+				{#key displayLocation || forceUpdate}
 					{#each $restaurantDatas as rd}
-						 {#if rd.coordinate.loc == displayLocation}
-							<Card name={rd.name} summary={rd.summary} price={rd.price} category={rd.category} />
+						{#if rd.coordinate.loc == displayLocation}
+							<Card name={rd.name} summary={rd.summary} price={rd.price} category={rd.category} detail={rd.detail}/>
 						{/if}
 					{/each}
 				{/key}
 			</div>
 		</div>
 	</div>
+	{/if}
 </main>
