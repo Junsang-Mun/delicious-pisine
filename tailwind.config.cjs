37da6efcb97ff4766b7fc90de9460c38bb42f1be
Junsang Mun <mun.js@aol.com>
2022-11-24 18:17:58 +0900

    [feature] chagne dark/light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
 		extend: {}
 	},
-
-	plugins: [require('daisyui')]
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["cmyk", "dracula", "halloween"],
+		darkTheme: "halloween",
+	}
 };
 
 module.exports = config;
