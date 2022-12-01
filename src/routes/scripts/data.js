c11767ee8c2472f31174c9f97d1b160b7b5039f0
Junsang Mun <mun.js@aol.com>
2022-12-01 15:33:47 +0900

    [refactoring] data.js 내 writable 추가 및 필요없는 함수 제거 리팩토링

--- src/routes/scripts/data.js
+++ src/routes/scripts/data.js
@@ -3,22 +3,17 @@ import { writable } from 'svelte/store';
 export function setLocation(location) {
 	if (location === 'gaepo') {
 		window.localStorage.setItem('location', 'gaepo');
+		currentLocation.set('gaepo');
 	} else if (location === 'seocho') {
 		window.localStorage.setItem('location', 'seocho');
+		currentLocation.set('seocho');
 	} else {
 		console.error('Unsupported location');
 		window.localStorage.setItem('location', 'gaepo');
-	}
-}
-
-export function getLocation() {
-	const loc = window.localStorage.getItem('location');
-	if (loc !== undefined) {
-		return loc;
-	} else {
-		return 'gaepo';
+		currentLocation.set('gaepo');
 	}
 }
 
 export const restaurantDatas = writable([]);
+export const currentLocation = writable('gaepo');
 export const currentView = writable('LIST');
